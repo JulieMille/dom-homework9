4f0171910a4df365787d28dbf5fe339bf45d7b6c
JulieMille <millesunflower@mail.ru>
2023-06-18 12:52:59 +0300

    creating API module

--- script.js
+++ script.js
@@ -112,7 +112,7 @@ const renderApp = (isInitialLoading, comentarios) => {
   </div>`
     appEl.innerHTML = appHtml;
 
-    document.querySelector(".login-button").addEventListener('click', () => {
+    document.querySelector('.login-button').addEventListener('click', () => {
       let token = "Bearer asb4c4boc86gasb4c4boc86g37w3cc3bo3b83k4g37k3bk3cg3c03ck4k";
       fetchGet();
       renderApp();
